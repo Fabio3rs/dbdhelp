1098c7c33584ebdce8d0577ae6bd41eaf70bdf6e
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2021-01-23 21:18:33 -0300

    Initial program updates testing source output

--- src/Program.cpp
+++ src/Program.cpp
@@ -1,5 +1,7 @@
 #include "Program.hpp"
 
+static int migrationtest = 0;
+
 int migmgr::Program::select_database(lua_State *L)
 {
 	CLuaFunctions::LuaParams p(L);
@@ -39,6 +41,7 @@ int migmgr::Program::create_table(lua_State *L)
 
         Program &pr = prog();
         int tblid = pr.dbs[dblid].create_table_if_not_exists(tblname);
+        pr.dbs[dblid].tables[tblid].migrations[0].identifier = ++migrationtest;
 
         p << tblid;
         //tbls.push_back({tblname, "CREATE"});
@@ -63,7 +66,8 @@ int migmgr::Program::alter_table(lua_State *L)
 
         Program &pr = prog();
         int tblid = pr.dbs[dblid].create_table_if_not_exists(tblname);
-        pr.dbs[dblid].tables[tblid].migrations.push_back({0, 0});
+        pr.dbs[dblid].tables[tblid].migrations.push_back({1, 0});
+        pr.dbs[dblid].tables[tblid].migrations.back().identifier = ++migrationtest;
 
         p << tblid;
 	}
@@ -167,11 +171,184 @@ int migmgr::Program::dumptbl(lua_State *L)
         std::cout << "Database: " << db.name << std::endl;
         std::cout << "Table: " << tb.name << std::endl;
         std::cout << "\tNAME\t\tTYPE" << std::endl;
+        
+        FILE *outmodel = fopen((tb.name + ".cs").c_str(), "w");
+
+        fprintf(outmodel, "class %s\n{", tb.name.c_str());
+        fprintf(outmodel, "\tbool[] changed = new bool[%ld];\n", tb.fields.size());
+        int fieldid = 0;
         for (auto &field : tb.fields)
         {
-            std::cout << "\t" << field.name << "\t\t" << field.ft << std::endl;
+            std::string varTypename;
+
+            switch(field.ft)
+            {
+            case integer:
+            /*  if (field.unsignedval) varTypename += "unsigned "*/
+                varTypename += "int";
+                break;
+
+            case string:
+                varTypename += "String";
+                break;
+
+            case decimal:
+                varTypename += "double";
+                break;
+
+            case date:
+            case datetime:
+                varTypename += "DateTime";
+                break;
+
+            case timestamp:
+                varTypename += "Int64";
+                break;
+            }
+
+            fprintf(outmodel, "\tprivate %s _%s; // %d\n", varTypename.c_str(), field.name.c_str(), fieldid);
+            fprintf(outmodel, "\tpublic %s %s {\n\t\tget { return _%s; }\n\t\tset { changed[%d] = true; _%s = value; }\n\t}\n", varTypename.c_str(), field.name.c_str(), field.name.c_str(), fieldid, field.name.c_str());
+
+            ++fieldid;
         }
 
+        fprintf(outmodel, "\n");
+        fprintf(outmodel, "\tpublic void save()\n\t{\n");
+        fieldid = 0;
+        fprintf(outmodel, "\t\tint paramnum = 0;\n\n");
+        fprintf(outmodel, "\t\tString strsqlupdate = \"UPDATE %s SET \";\n", tb.name.c_str());
+        for (auto &field : tb.fields)
+        {
+            if (field.fg != primary_key)
+            {
+                fprintf(outmodel, "\t\tif (changed[%d])\n\t\t{\n", fieldid);
+                fprintf(outmodel, "\t\t\tif (paramnum != 0)\n\t\t\t{\n");
+                fprintf(outmodel, "\t\t\t\tstrsqlupdate += \", \";\n");
+                fprintf(outmodel, "\t\t\t}\n\n");
+                fprintf(outmodel, "\t\t\tstrsqlupdate += \"%s = @param\" + paramnum.ToString();\n", field.name.c_str());
+                fprintf(outmodel, "\t\t\tparamnum++;\n");
+                fprintf(outmodel, "\t\t}\n\n");
+            }
+
+            ++fieldid;
+        }
+        fprintf(outmodel, "\t\tstrsqlupdate +=  \" WHERE id = \" + id.ToString();\n");
+
+        fprintf(outmodel, "\t\tSqlCommand sqlccmd = new SqlCommand(strsqlupdate, sqlconn);\n");
+
+        fieldid = 0;
+        fprintf(outmodel, "\t\tparamnum = 0;\n\n");
+        for (auto &field : tb.fields)
+        {
+            if (field.fg != primary_key)
+            {
+                fprintf(outmodel, "\t\tif (changed[%d])\n\t\t{\n", fieldid);
+                fprintf(outmodel, "\t\t\tsqlccmd.Parameters.AddWithValue(\"@param\" + paramnum.ToString(), %s);\n", field.name.c_str());
+                fprintf(outmodel, "\t\t\tparamnum++;\n");
+                fprintf(outmodel, "\t\t}\n\n");
+            }
+
+            ++fieldid;
+        }
+        
+        fprintf(outmodel, "\t\tConsole.WriteLine(sqlccmd.ExecuteNonQuery().ToString());\n\n");
+
+        fprintf(outmodel, "\t}\n\n");
+
+
+        fprintf(outmodel, "\tpublic void create()\n\t{\n");
+        fprintf(outmodel, "\t\tSqlCommand cmdsqlcreate = new SqlCommand(\"INSERT INTO %s (", tb.name.c_str());
+
+        bool comma = false;
+        for (auto &field : tb.fields)
+        {
+            if (field.fg != primary_key)
+            {
+                if (comma)
+                {
+                    fprintf(outmodel, ", ");
+                }
+                comma = true;
+                fprintf(outmodel, "%s", field.name.c_str());
+            }
+        }
+        fprintf(outmodel, ") VALUES (");
+        comma = false;
+
+        fieldid = 0;
+        for (auto &field : tb.fields)
+        {
+            if (field.fg != primary_key)
+            {
+                if (comma)
+                {
+                    fprintf(outmodel, ", ");
+                }
+                comma = true;
+                fprintf(outmodel, "@param%d", fieldid);
+            }
+            
+            ++fieldid;
+        }
+        fprintf(outmodel, "); SELECT SCOPE_IDENTITY()\", sqlconn);\n");
+
+
+        fieldid = 0;
+        for (auto &field : tb.fields)
+        {
+            if (field.fg != primary_key)
+            {
+                fprintf(outmodel, "\t\tcmdsqlcreate.Parameters.AddWithValue(\"@param%d\", %s);\n", fieldid, field.name.c_str());
+            }
+
+            ++fieldid;
+        }                            
+        fprintf(outmodel, "\t\t_id = Convert.ToInt32(cmdsqlcreate.ExecuteScalar());\n");
+
+        fprintf(outmodel, "\t}\n\n");
+        fprintf(outmodel, "}\n\n");
+
+        fclose(outmodel);
+        outmodel = nullptr;
+
+        FILE *outpcs = fopen(("migrate" + tb.name + ".cs").c_str(), "w");
+
+        for (auto &migration : tb.migrations)
+        {
+            fprintf(outpcs, "if (lastmigration < %d)\n{\n", migration.identifier);
+            std::string nm = (migration.migtype? "alter" : "create");
+            nm += "_for_" + tb.name;
+            fprintf(outpcs, "TableField[] %s = new TableField[]\n{\n", nm.c_str());
+            for (auto &field : migration.fields)
+            {
+                //  TableField(String n, int t, int l, bool nb, bool pk, bool ind, bool uns, bool ai)
+                fprintf(outpcs, "    new TableField(\"%s\", %s, %d, %s, %s, %s, %s, %s),\n",
+                    field.name.c_str(), std::to_string(field.ft).c_str(), field.ft == string? 255 : 0,  field.fg == primary_key? "false" : "true",
+                    field.fg == primary_key? "true" : "false",
+                    field.fg == primary_key? "true" : "false",
+                    field.fg == primary_key? "true" : "false",
+                    field.fg == primary_key? "true" : "false");
+            }
+
+            switch (migration.migtype)
+            {
+            case 0:
+                fprintf(outpcs, "};\nsql.create_table(\"%s\", %s);\n", tb.name.c_str(), nm.c_str());
+                break;
+
+            case 1:
+                fprintf(outpcs, "};\nsql.alter_table(\"%s\", %s);\n", tb.name.c_str(), nm.c_str());
+                break;
+
+            default:
+                fprintf(outpcs, "error\n");
+                break;
+            }
+            fprintf(outpcs, "}\n\n");
+        }
+
+        fclose(outpcs);
+        outpcs = 0;
         //tbls.push_back({tblname, "CREATE"});
         //p << tbls.size() - 1;
 	}
